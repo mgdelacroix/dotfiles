3613f748b74b606ee378def3afeeefa248686e56
Miguel de la Cruz <mgdelacroix@gmail.com>
2014-08-18 17:08:31 +0200

    Vertical monitors script added

---
+++ bin/vertical.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 1200x1080 --rotate normal --output VGA1 --mode 1920x1200 --pos 0x0 --rotate left
+feh --bg-fill ~/Pictures/wallpaper
